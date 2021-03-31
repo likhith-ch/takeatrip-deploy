dfe5995e256758c97b4377c5d7f624ed90a19032
likhith <chilukurthilikhith@gmail.com>
2021-03-31 10:34:00 +0530

    Hotels page modified UX

--- src/app/hotels/hotels.component.ts
+++ src/app/hotels/hotels.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
-
+selectedcity=""
+checkindate:any
+checkoutdate:any
+roomscount:any
   constructor() { }
+ ngOnInit():any{
+  console.log(this.selectedcity)
+ }
+cities=["Hyderabad","Chennai","Vijayawada","Vishakapatnam"
+]
 
-  ngOnInit(): void {
+  onchange(){
+    console.log(this.selectedcity)
+    console.log(this.checkindate)
+    console.log(this.checkoutdate)
+    console.log(this.roomscount)
   }
-
+  
 }
